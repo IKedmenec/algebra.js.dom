f793eb4f709a8b36a350e511ef100fe7eafb763c
IKedmenec <ivan.kec86@gmail.com>
2024-05-06 19:33:13 +0200

    promjena4a

--- index2.js
+++ index2.js
@@ -2,11 +2,11 @@ var h1 = document.createElement("h1");
 h1.appendChild(document.createTextNode("Ovo je text unutar h1"));
 
 var dDescr = document.querySelector(".description");
-console.log("dDescr--> " + dDescr);
-
 dDescr.parentNode.prepend(h1);
 
-var h2 = document.createElement("h2");
-h2.appendChild(document.createTextNode)("Ovo je tekst unutar h2");
+/*var divInfo = document.createElement("div");
+divInfo.classList.add("info");
 
-dDescr.parentNode.append(h2);
+dDescr.parentNode.insertBefore(divInfo,dDescr,nextSibling);*/
+
+
