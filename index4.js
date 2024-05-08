a28a32a00a9c0777bc48d1a91fc3627947cc4f99
IKedmenec <ivan.kec86@gmail.com>
2024-05-08 19:07:40 +0200

    promjena 87a

--- index4.js
+++ index4.js
@@ -1,2 +1,13 @@
 var ul = document.querySelector("ul");
-ul.style.type = "decimal";
+// jedan od nacina je da UL elementu dodjelimo syle "decimal"
+// ul.style.listStyle = "decimal";
+
+// drugi nacin je da iteriramo kroz UL i svakom od LI elemenata promjenimo svojstvo na "decimal"
+
+var i = 1;
+for (let elem of ul.children){
+    elem.style.listStyle = "decimal";
+    //elem.textContent = i + ". " + elem.textContent;
+
+    i++;
+}
