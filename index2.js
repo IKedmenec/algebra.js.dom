d3a223b8649b1c263e793b7b31db484d98238694
IKedmenec <ivan.kec86@gmail.com>
2024-05-08 17:14:17 +0200

    promjene 5a

--- index2.js
+++ index2.js
@@ -1,12 +1,53 @@
 var h1 = document.createElement("h1");
 h1.appendChild(document.createTextNode("Ovo je text unutar h1"));
 
-var dDescr = document.querySelector(".description");
-dDescr.parentNode.prepend(h1);
+/*
+var dDescr = document.querySelector("body");
+console.log(bodyElem);
 
-/*var divInfo = document.createElement("div");
+//vodyElem.prepend(h1); 
+*/
+
+var dDescr = document.querySelector(".description");
+dDescr.parentNode.prepend(h1); 
+
+// kreiramo novi objekt u memoriji div sa class atributom i vrijednosti info
+// <div class="info"></div> 
+
+var divInfo = document.createElement("div");
 divInfo.classList.add("info");
 
-dDescr.parentNode.insertBefore(divInfo,dDescr,nextSibling);*/
+// nalazimo element sa id=footer
+// insertamo na body novi kreirani objekt i inseratamo prije footer elementa
+
+var footElement = document.querySelector("#footer");
+footElement.parentNode.insertBefore(divInfo, footElement); 
+
+// trebamo naci <ul> element
+
+var ul = document.querySelector("ul"); // dohvacen iz memorije (DOM-a)
+// var li = document.createElement("li"); // kereiramo novi objekt u memoriji
+
+for (let i = 0; i < 3; i++) {
+    let li = document.createElement("li");
+    ul.appendChild(li);
+}
+
+/* var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+
+ul.appendChild(li);
+ul.appendChild(li1);
+ul.appendChild(li2);
+*/
+
+/*
+var divInfo = document.createElement("div");
+divInfo.classList.add("info");
+
+dDescr.parentNode.insertBefore(divInfo,dDescr,nextSibling); 
+*/
+
+
 
 
