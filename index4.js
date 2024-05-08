f3e8836e596b0bcbcfbaaa3abafc390b858f5df5
IKedmenec <ivan.kec86@gmail.com>
2024-05-08 19:19:31 +0200

    promjena333

--- index4.js
+++ index4.js
@@ -5,9 +5,13 @@ var ul = document.querySelector("ul");
 // drugi nacin je da iteriramo kroz UL i svakom od LI elemenata promjenimo svojstvo na "decimal"
 
 var i = 1;
-for (let elem of ul.children){
+for (let elem of ul.children) {
     elem.style.listStyle = "decimal";
     //elem.textContent = i + ". " + elem.textContent;
-
     i++;
 }
+
+var divInfo = document.querySelector(".info");
+divInfo.classList.add("sakrij");
+
+var body
