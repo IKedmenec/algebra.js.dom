f402106c342de39fe9934a9ddc6f6abb9c11a3cc
IKedmenec <ivan.kec86@gmail.com>
2024-05-16 18:36:46 +0200

    promjena200b

---
+++ koristenjeObrasci.js
@@ -0,0 +1,22 @@
+var brojanje = require('./obrasci');
+
+// krenuli smo total walk = 0
+
+console.log("Total steps = " + brojanje.getTotalSteps()); // 0
+
+brojanje.walk();
+
+console.log("Total steps = " + brojanje.getTotalSteps()); // 1
+
+brojanje.walk();
+
+console.log("Total steps = " + brojanje.getTotalSteps()); // 2
+
+brojanje.walk();
+
+console.log("Total steps = " + brojanje.getTotalSteps()); // 3
+
+brojanje.run();
+
+var total = brojanje.getTotalSteps();
+console.log("Total steps = " + total); 
